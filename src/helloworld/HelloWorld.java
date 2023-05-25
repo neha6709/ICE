4ce14369b06aa9b3dd9c97edeb4bd6c4819dad3b
neha6709 <neha22@sheridancollege.ca>
2023-05-25 09:47:50 -0400

    hello

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,28 @@
+
+package helloworld;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Neha
+ */
+public class HelloWorld {
+
+    
+    public static void main(String[] args) {
+       
+    String name;
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter name:" );
+    name = sc.nextLine();
+    char[] revString = new char[name.length()]; // Create a char array of the same length as the word
+    for (int i = 0; i < name.length(); i++) {
+        revString[i] = name.charAt(i); // Store each character in the array
+    }
+    for (int i = revString.length - 1; i >= 0; i--) {
+        System.out.print(revString[i]); // Iterate backwards over the array and print each character
+    }
+}
+}
